b916c83a263c588d23ed044ab363c40645c39f97
JesusfGarcia <jesusfgarciavaldez@gmail.com>
2020-03-03 22:52:46 -0700

    refactor to drawline function

--- main.js
+++ main.js
@@ -114,60 +114,28 @@ drawLine = (line, result) => {
 		: null
 	switch (line) {
 		case 0:
-			document.getElementById('btn1').style.color = 'red'
-			document.getElementById('btn2').style.color = 'red'
-			document.getElementById('btn3').style.color = 'red'
-			document.getElementById('labelResult').innerText = ganador
-			end = true
+			redLetter('btn1', 'btn2', 'btn3', ganador)
 			break
 		case 1:
-			document.getElementById('btn4').style.color = 'red'
-			document.getElementById('btn5').style.color = 'red'
-			document.getElementById('btn6').style.color = 'red'
-			document.getElementById('labelResult').innerText = ganador
-			end = true
+			redLetter('btn4', 'btn5', 'btn6', ganador)
 			break
 		case 2:
-			document.getElementById('btn7').style.color = 'red'
-			document.getElementById('btn8').style.color = 'red'
-			document.getElementById('btn9').style.color = 'red'
-			document.getElementById('labelResult').innerText = ganador
-			end = true
+			redLetter('btn7', 'btn8', 'btn9', ganador)
 			break
 		case 3:
-			document.getElementById('btn1').style.color = 'red'
-			document.getElementById('btn4').style.color = 'red'
-			document.getElementById('btn7').style.color = 'red'
-			document.getElementById('labelResult').innerText = ganador
-			end = true
+			redLetter('btn1', 'btn4', 'btn7', ganador)
 			break
 		case 4:
-			document.getElementById('btn2').style.color = 'red'
-			document.getElementById('btn5').style.color = 'red'
-			document.getElementById('btn8').style.color = 'red'
-			document.getElementById('labelResult').innerText = ganador
-			end = true
+			redLetter('btn2', 'btn5', 'btn8', ganador)
 			break
 		case 5:
-			document.getElementById('btn3').style.color = 'red'
-			document.getElementById('btn6').style.color = 'red'
-			document.getElementById('btn9').style.color = 'red'
-			document.getElementById('labelResult').innerText = ganador
-			end = true
+			redLetter('btn3', 'btn6', 'btn9', ganador)
 			break
 		case 6:
-			document.getElementById('btn1').style.color = 'red'
-			document.getElementById('btn5').style.color = 'red'
-			document.getElementById('btn9').style.color = 'red'
-			document.getElementById('labelResult').innerText = ganador
-			end = true
+			redLetter('btn1', 'btn5', 'btn9', ganador)
 			break
 		case 7:
-			document.getElementById('btn3').style.color = 'red'
-			document.getElementById('btn5').style.color = 'red'
-			document.getElementById('btn7').style.color = 'red'
-			document.getElementById('labelResult').innerText = ganador
-			end = true
+			redLetter('btn3', 'btn5', 'btn7', ganador)
 			break
 		case 8:
 			if (end === false) {
@@ -177,6 +145,14 @@ drawLine = (line, result) => {
 		default:
 			break
 	}
+}
+
+redLetter = (id1, id2, id3, ganador) => {
+	document.getElementById(id1).style.color = 'red'
+	document.getElementById(id2).style.color = 'red'
+	document.getElementById(id3).style.color = 'red'
+	document.getElementById('labelResult').innerText = ganador
+	end = true
 }
 
 reset = () => {
